5cfad4c0e5df5fd1fbf7220a546f4a67f2f2e8cf
anup-paul <anuppaul1845@gmail.com>
2021-05-26 04:47:59 +0600

    adding-list

--- src/pages/Discover.js
+++ src/pages/Discover.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Book from '../components/Book/Book';
-import books from '../fakeData/books.json'
+// import books from '../fakeData/books.json'
+import {useSelector} from 'react-redux'
 import PageLayout from '../components/PageLayout/PageLayout';
 const Discover = () => {
+    const books = useSelector((state) => {
+        //console.log(state.books.discoverList);
+        return state.books.discoverList;
+    })
     return (
         <PageLayout>
             {
-                books.map((book) => (<Book key={book.id} book={book} />))
+                books?.map((book) => (<Book key={book.id} book={book} />))
             }
         </PageLayout>
     );
